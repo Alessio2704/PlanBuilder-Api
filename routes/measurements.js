5139ecb32756db484a343387428a8eca280a1646
Alessio2704 <marcuzzial@gmail.com>
2022-06-03 11:44:53 +0200

    u

--- routes/measurements.js
+++ routes/measurements.js
@@ -9,33 +9,8 @@ router.post("/add/:id", verify, (req, res) => {
             const client = user.clients.filter(function (clientDB) {
                 return clientDB.name === req.body.name && clientDB.surname === req.body.surname && clientDB.phoneNumber === req.body.phoneNumber;
             }).pop();
-
-            const newMeasurement = {
-                weight: req.body.weight,
-                ankleDx: req.body.ankleDx,
-                ankleSx: req.body.ankleSx,
-                calfDx: req.body.calfDx,
-                calfSx: req.body.calfSx,
-                upperThighDx: req.body.upperThighDx,
-                upperThighSx: req.body.upperThighSx,
-                middleThighDx: req.body.middleThighDx,
-                middleThighSx: req.body.middleThighSx,
-                flanks: req.body.flanks,
-                lowerBellyButton: req.body.lowerBellyButton,
-                bellyButton: req.body.bellyButton,
-                waist: req.body.waist,
-                chest: req.body.chest,
-                neck: req.body.neck,
-                armDx: req.body.armDx,
-                armSx: req.body.armSx,
-                wristDx: req.body.wristDx,
-                wristSx: req.body.wristSx,
-                date: Date.now
-            };
-
-            console.log(newMeasurement);
-
-            client.measurements.push(newMeasurement);
+            
+            client.measurements.push(req.body.newMeasurement);
 
             user.save();
 
