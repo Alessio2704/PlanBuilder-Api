0ecfbbcdb7b5dd5d4ce5df41eedd22f0e2334943
Alessio2704 <marcuzzial@gmail.com>
2022-06-03 12:25:21 +0200

    u

--- routes/measurements.js
+++ routes/measurements.js
@@ -10,9 +10,7 @@ router.post("/add/:id", verify, (req, res) => {
                 return clientDB.name === req.body.name && clientDB.surname === req.body.surname && clientDB.phoneNumber === req.body.phoneNumber;
             }).pop();
             
-            console.log(req.body.newMeasurement);
             client.measurements.push(req.body.newMeasurement);
-            console.log(client.measurements[0]);
             user.save();
 
             res.send({"message":"New measurement added succesfully"})
@@ -20,6 +18,33 @@ router.post("/add/:id", verify, (req, res) => {
         } catch(err) {
             console.log(err)
             res.status(404).send({"message":"Unable to add measurement"});
+        }
+    });
+});
+
+router.put("/info/:id", verify, (req, res) => {
+
+    const user = User.findOne({_id:req.params.id}, function (err, user) {
+        try {
+            const client = user.clients.filter(function (clientDB) {
+                return clientDB.name === req.body.name && clientDB.surname === req.body.surname && clientDB.phoneNumber === req.body.phoneNumber;
+            }).pop();
+            
+            const result = [];
+
+            for (i in client.measurements) {
+                const measurementResult = {
+                    "date": client.measurements[i].date
+                }
+
+                result.push(measurementResult);
+            }
+
+            res.send(result);
+
+        } catch(err) {
+            console.log(err)
+            res.status(404).send({"message":"Unable to get measurement"});
         }
     });
 });
